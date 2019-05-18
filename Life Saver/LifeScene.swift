08d2762e96ab813a7358c86d5f38bce7ec0b1335
amiantos <bradroot@me.com>
2019-05-18 10:33:14 -0700

    extremely inefficient initial implementation of life

--- Life Saver/LifeScene.swift
+++ Life Saver/LifeScene.swift
@@ -9,10 +9,18 @@
 import GameplayKit
 import SpriteKit
 
-struct SquareNodeData {
+class SquareNodeData {
     let x: Int
     let y: Int
     let node: SKShapeNode
+    var alive: Bool
+
+    init(x: Int, y: Int, node: SKShapeNode, alive: Bool) {
+        self.x = x
+        self.y = y
+        self.node = node
+        self.alive = alive
+    }
 }
 
 class LifeScene: SKScene {
@@ -33,8 +41,8 @@ class LifeScene: SKScene {
         camera = cameraNode
 
         // Try drawing some squares...
-        let lengthSquares: CGFloat = 12
-        let heightSquares: CGFloat = 20
+        let lengthSquares: CGFloat = 10
+        let heightSquares: CGFloat = 10
         let totalSquares: CGFloat = lengthSquares * heightSquares
         let squareWidth: CGFloat = size.width / lengthSquares
         let squareHeight: CGFloat = size.height / heightSquares
@@ -50,14 +58,18 @@ class LifeScene: SKScene {
 //            newSquare.strokeColor = .black
             addChild(newSquare)
             newSquare.position = CGPoint(x: nextXPosition, y: nextYPosition)
-            let labelNode = SKLabelNode(text: "\(Int(createdSquares))")
-            labelNode.fontColor = .white
-            newSquare.addChild(labelNode)
-            labelNode.position = CGPoint(x: squareWidth / 2, y: squareHeight / 2)
+//            let labelNode = SKLabelNode(text: "\(Int(createdSquares))")
+//            labelNode.fontColor = .white
+//            newSquare.addChild(labelNode)
+//            labelNode.position = CGPoint(x: squareWidth / 2, y: squareHeight / 2)
 
-            let newSquareData = SquareNodeData(x: nextXValue, y: nextYValue, node: newSquare)
+            let livingChoices = [true, false]
+            let newSquareData = SquareNodeData(x: nextXValue, y: nextYValue, node: newSquare, alive: livingChoices.randomElement()!)
+            if newSquareData.alive {
+                newSquare.fillColor = SKColor(red: 173/255.0, green: 98/255.0, blue: 22/255.0, alpha: 1.00)
+            }
             squareData.append(newSquareData)
-            labelNode.text = "(\(newSquareData.x), \(newSquareData.y))"
+//            labelNode.text = "(\(newSquareData.x), \(newSquareData.y))"
 
             createdSquares += 1
             squareNodes.append(newSquare)
@@ -82,34 +94,61 @@ class LifeScene: SKScene {
 
     override func update(_ currentTime: TimeInterval) {
         if lastUpdate == 0 { lastUpdate = currentTime }
-//        if currentTime - lastUpdate >= 0.5 {
-//            let colors: [SKColor] = [
-//                SKColor(red: 2/255.0, green: 30/255.0, blue: 50/255.0, alpha: 1.00),
-//                SKColor(red: 173/255.0, green: 98/255.0, blue: 22/255.0, alpha: 1.00),
-//                SKColor(red: 14/255.0, green: 4/255.0, blue: 0/255.0, alpha: 1.00),
-//                SKColor(red: 14/255.0, green: 11/255.0, blue: 1/255.0, alpha: 1.00),
-//                SKColor(red: 14/255.0, green: 8/255.0, blue: 0/255.0, alpha: 1.00),
-//                SKColor(red: 0/255.0, green: 5/255.0, blue: 9/255.0, alpha: 1.00),
-//                SKColor(red: 3/255.0, green: 49/255.0, blue: 82/255.0, alpha: 1.00),
-//                SKColor(red: 172/255.0, green: 48/255.0, blue: 17/255.0, alpha: 1.00),
-//                SKColor(red: 174/255.0, green: 129/255.0, blue: 0/255.0, alpha: 1.00),
-//                SKColor(red: 80/255.0, green: 22/255.0, blue: 0/255.0, alpha: 1.00),
-//                SKColor(red: 80/255.0, green: 58/255.0, blue: 7/255.0, alpha: 1.00),
-//                SKColor(red: 79/255.0, green: 45/255.0, blue: 6/255.0, alpha: 1.00),
-//                SKColor(red: 128/255.0, green: 95/255.0, blue: 17/255.0, alpha: 1.00),
-//                SKColor(red: 6/255.0, green: 66/255.0, blue: 110/255.0, alpha: 1.00),
-//                SKColor(red: 129/255.0, green: 35/255.0, blue: 0/255.0, alpha: 1.00),
-//                SKColor(red: 128/255.0, green: 73/255.0, blue: 14/255.0, alpha: 1.00),
-//            ]
-//
+        if currentTime - lastUpdate >= 0.5 {
+            let colors: [SKColor] = [
+                SKColor(red: 173/255.0, green: 98/255.0, blue: 22/255.0, alpha: 1.00),
+                SKColor(red: 172/255.0, green: 48/255.0, blue: 17/255.0, alpha: 1.00),
+                SKColor(red: 174/255.0, green: 129/255.0, blue: 0/255.0, alpha: 1.00),
+                SKColor(red: 80/255.0, green: 22/255.0, blue: 0/255.0, alpha: 1.00),
+                SKColor(red: 80/255.0, green: 58/255.0, blue: 7/255.0, alpha: 1.00),
+                SKColor(red: 79/255.0, green: 45/255.0, blue: 6/255.0, alpha: 1.00),
+                SKColor(red: 128/255.0, green: 95/255.0, blue: 17/255.0, alpha: 1.00),
+                SKColor(red: 129/255.0, green: 35/255.0, blue: 0/255.0, alpha: 1.00),
+                SKColor(red: 128/255.0, green: 73/255.0, blue: 14/255.0, alpha: 1.00),
+            ]
+
 //            for _ in 0..<squareNodes.count / 10 {
 //                let node = squareNodes.randomElement()!
 //                let color = colors.randomElement() ?? .black
 //                node.fillColor = color
-//                node.strokeColor = color
+////                node.strokeColor = color
 //            }
-//            lastUpdate = currentTime
-//        }
+
+            for nodeData in squareData {
+                print("Current Node: (\(nodeData.x), \(nodeData.y))")
+
+                // Get neighbors...
+                let livingNeighbors = squareData.filter {
+                    (($0.x == (nodeData.x + 1) && $0.y == (nodeData.y + 1))
+                    || ($0.x == (nodeData.x - 1) && $0.y == (nodeData.y - 1))
+                    || ($0.x == (nodeData.x + 1) && $0.y == (nodeData.y - 1))
+                    || ($0.x == (nodeData.x - 1) && $0.y == (nodeData.y + 1))
+                    || ($0.x == (nodeData.x) && $0.y == (nodeData.y + 1))
+                    || ($0.x == (nodeData.x) && $0.y == (nodeData.y - 1))
+                    || ($0.x == (nodeData.x - 1) && $0.y == (nodeData.y))
+                    || ($0.x == (nodeData.x + 1) && $0.y == (nodeData.y)))
+                    && $0.alive
+                }
+
+                if nodeData.alive {
+                    if livingNeighbors.count > 3 {
+                        nodeData.alive = false
+                    } else if livingNeighbors.count < 2 {
+                        nodeData.alive = false
+                    }
+                } else if livingNeighbors.count == 3 {
+                    nodeData.alive = true
+                }
+
+                if nodeData.alive {
+                    nodeData.node.fillColor = SKColor(red: 173/255.0, green: 98/255.0, blue: 22/255.0, alpha: 1.00)
+                } else {
+                    nodeData.node.fillColor = .black
+                }
+            }
+
+            lastUpdate = currentTime
+        }
     }
 
 }
