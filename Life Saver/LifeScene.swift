72d6059fc00e74c5f6622e375c1cd9dd1e305d00
amiantos <bradroot@me.com>
2019-05-18 08:52:12 -0700

    put in some nice colors just for testing

--- Life Saver/LifeScene.swift
+++ Life Saver/LifeScene.swift
@@ -37,12 +37,13 @@ class LifeScene: SKScene {
         while createdSquares < totalSquares {
             let newSquare = SKShapeNode(rect: CGRect(x: 0, y: 0, width: squareWidth, height: squareHeight))
             newSquare.fillColor = .black
+            newSquare.strokeColor = .black
             addChild(newSquare)
             newSquare.position = CGPoint(x: nextXPosition, y: nextYPosition)
-            let labelNode = SKLabelNode(text: "\(Int(createdSquares))")
-            labelNode.fontColor = .white
-            newSquare.addChild(labelNode)
-            labelNode.position = CGPoint(x: squareWidth / 2, y: squareHeight / 2)
+//            let labelNode = SKLabelNode(text: "\(Int(createdSquares))")
+//            labelNode.fontColor = .white
+//            newSquare.addChild(labelNode)
+//            labelNode.position = CGPoint(x: squareWidth / 2, y: squareHeight / 2)
 
             createdSquares += 1
             squareNodes.append(newSquare)
@@ -67,17 +68,27 @@ class LifeScene: SKScene {
         if lastUpdate == 0 { lastUpdate = currentTime }
         if currentTime - lastUpdate >= 1 {
             let colors: [SKColor] = [
-                .black,
-                .blue,
-                .brown,
-                .green,
-                .red,
-                .purple,
-                .white
+                SKColor(red: 2/255.0, green: 30/255.0, blue: 50/255.0, alpha: 1.00),
+                SKColor(red: 173/255.0, green: 98/255.0, blue: 22/255.0, alpha: 1.00),
+                SKColor(red: 14/255.0, green: 4/255.0, blue: 0/255.0, alpha: 1.00),
+                SKColor(red: 14/255.0, green: 11/255.0, blue: 1/255.0, alpha: 1.00),
+                SKColor(red: 14/255.0, green: 8/255.0, blue: 0/255.0, alpha: 1.00),
+                SKColor(red: 0/255.0, green: 5/255.0, blue: 9/255.0, alpha: 1.00),
+                SKColor(red: 3/255.0, green: 49/255.0, blue: 82/255.0, alpha: 1.00),
+                SKColor(red: 172/255.0, green: 48/255.0, blue: 17/255.0, alpha: 1.00),
+                SKColor(red: 174/255.0, green: 129/255.0, blue: 0/255.0, alpha: 1.00),
+                SKColor(red: 80/255.0, green: 22/255.0, blue: 0/255.0, alpha: 1.00),
+                SKColor(red: 80/255.0, green: 58/255.0, blue: 7/255.0, alpha: 1.00),
+                SKColor(red: 79/255.0, green: 45/255.0, blue: 6/255.0, alpha: 1.00),
+                SKColor(red: 128/255.0, green: 95/255.0, blue: 17/255.0, alpha: 1.00),
+                SKColor(red: 6/255.0, green: 66/255.0, blue: 110/255.0, alpha: 1.00),
+                SKColor(red: 129/255.0, green: 35/255.0, blue: 0/255.0, alpha: 1.00),
+                SKColor(red: 128/255.0, green: 73/255.0, blue: 14/255.0, alpha: 1.00),
             ]
             for child in squareNodes {
-                print(child)
-                child.fillColor = colors.randomElement()!
+                let color = colors.randomElement() ?? .black
+                child.fillColor = color
+                child.strokeColor = color
             }
             lastUpdate = currentTime
         }
