95538dcee6728de912ff91bb4f9019c56f7479b0
amiantos <bradroot@me.com>
2019-05-25 22:09:53 -0700

    make it easier to switch presets for macOS (screenshot generation)

--- Life Saver macOS/ViewController.swift
+++ Life Saver macOS/ViewController.swift
@@ -18,8 +18,19 @@ class ViewController: NSViewController {
 
         let scene = LifeScene(size: view.frame.size)
 
+
         scene.animationSpeed = .fast
-        scene.squareSize = .large
+        scene.squareSize = .medium
+
+        if let preset = lifePresets.filter({ $0.title == "Georgia" }).first {
+            if let appearanceMode = preset.appearanceMode {
+                scene.appearanceMode = appearanceMode
+            }
+            if let color1 = preset.color1, let color2 = preset.color2, let color3 = preset.color3 {
+                scene.aliveColors = [color1, color2, color3]
+            }
+        }
+
 
         let skView = view as? SKView
         skView?.presentScene(scene)
