d852da339aa4c8a4df99cde08952a7f974cf396d
amiantos <bradroot@me.com>
2019-05-25 17:00:10 -0700

    add more color schemes, and rename some existing ones

--- Life Saver Screensaver/Configuration/ConfigureSheetController.swift
+++ Life Saver Screensaver/Configuration/ConfigureSheetController.swift
@@ -44,7 +44,7 @@ final class ConfigureSheetController: NSObject {
         ),
         LifeSettings(
             title: "Colors: Santa Fe",
-            appearanceMode: nil,
+            appearanceMode: .dark,
             squareSize: nil,
             animationSpeed: nil,
             color1: SKColor.defaultColor1,
@@ -52,8 +52,8 @@ final class ConfigureSheetController: NSObject {
             color3: SKColor.defaultColor3
         ),
         LifeSettings(
-            title: "Colors: Spooky",
-            appearanceMode: nil,
+            title: "Colors: Braineater",
+            appearanceMode: .dark,
             squareSize: nil,
             animationSpeed: nil,
             color1: SKColor(red: 103 / 255.0, green: 22 / 255.0, blue: 169 / 255.0, alpha: 1.00),
@@ -61,8 +61,17 @@ final class ConfigureSheetController: NSObject {
             color3: SKColor(red: 12 / 255.0, green: 67 / 255.0, blue: 108 / 255.0, alpha: 1.00)
         ),
         LifeSettings(
+            title: "Colors: Reign In Blood",
+            appearanceMode: .dark,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 113 / 255.0, green: 17 / 255.0, blue: 8 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 95 / 255.0, green: 7 / 255.0, blue: 0 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 55 / 255.0, green: 55 / 255.0, blue: 55 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
             title: "Colors: Swamp Girl",
-            appearanceMode: nil,
+            appearanceMode: .dark,
             squareSize: nil,
             animationSpeed: nil,
             color1: SKColor(red: 173 / 255.0, green: 255 / 255.0, blue: 14 / 255.0, alpha: 1.00),
@@ -70,8 +79,8 @@ final class ConfigureSheetController: NSObject {
             color3: SKColor(red: 6 / 255.0, green: 66 / 255.0, blue: 110 / 255.0, alpha: 1.00)
         ),
         LifeSettings(
-            title: "Colors: Lost Boys",
-            appearanceMode: nil,
+            title: "Colors: This Is America",
+            appearanceMode: .dark,
             squareSize: nil,
             animationSpeed: nil,
             color1: SKColor(red: 190 / 255.0, green: 14 / 255.0, blue: 19 / 255.0, alpha: 1.00),
@@ -79,7 +88,7 @@ final class ConfigureSheetController: NSObject {
             color3: SKColor(red: 212 / 255.0, green: 205 / 255.0, blue: 196 / 255.0, alpha: 1.00)
         ),
         LifeSettings(
-            title: "Colors: Noun Project",
+            title: "Colors: The Noun Project",
             appearanceMode: .dark,
             squareSize: nil,
             animationSpeed: nil,
@@ -95,6 +104,87 @@ final class ConfigureSheetController: NSObject {
             color1: SKColor(red: 252 / 255.0, green: 98 / 255.0, blue: 101 / 255.0, alpha: 1.00),
             color2: SKColor(red: 88 / 255.0, green: 137 / 255.0, blue: 251 / 255.0, alpha: 1.00),
             color3: SKColor(red: 38 / 255.0, green: 205 / 255.0, blue: 105 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Deuteranopia",
+            appearanceMode: .dark,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 211 / 255.0, green: 208 / 255.0, blue: 203 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 88 / 255.0, green: 123 / 255.0, blue: 127 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 255 / 255.0, green: 173 / 255.0, blue: 105 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Retro Pastel",
+            appearanceMode: .light,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 229 / 255.0, green: 167 / 255.0, blue: 177 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 244 / 255.0, green: 243 / 255.0, blue: 216 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 175 / 255.0, green: 211 / 255.0, blue: 213 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Better Days",
+            appearanceMode: .dark,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 95 / 255.0, green: 67 / 255.0, blue: 107 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 205 / 255.0, green: 170 / 255.0, blue: 37 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 114 / 255.0, green: 100 / 255.0, blue: 87 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Bubblegum",
+            appearanceMode: .light,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 1 / 255.0, green: 153 / 255.0, blue: 138 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 255 / 255.0, green: 203 / 255.0, blue: 213 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 191 / 255.0, green: 178 / 255.0, blue: 95 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Georgia",
+            appearanceMode: .light,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 237 / 255.0, green: 200 / 255.0, blue: 195 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 16 / 255.0, green: 103 / 255.0, blue: 110 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 247 / 255.0, green: 172 / 255.0, blue: 153 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Boysenberry",
+            appearanceMode: .dark,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 122 / 255.0, green: 55 / 255.0, blue: 100 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 56 / 255.0, green: 66 / 255.0, blue: 109 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 160 / 255.0, green: 121 / 255.0, blue: 72 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Tal Véz",
+            appearanceMode: .dark,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 59 / 255.0, green: 67 / 255.0, blue: 72 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 247 / 255.0, green: 201 / 255.0, blue: 177 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 176 / 255.0, green: 197 / 255.0, blue: 223 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Deep Forest",
+            appearanceMode: .dark,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 73 / 255.0, green: 110 / 255.0, blue: 28 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 108 / 255.0, green: 118 / 255.0, blue: 30 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 22 / 255.0, green: 87 / 255.0, blue: 48 / 255.0, alpha: 1.00)
+        ),
+        LifeSettings(
+            title: "Colors: Bite Me",
+            appearanceMode: .dark,
+            squareSize: nil,
+            animationSpeed: nil,
+            color1: SKColor(red: 243 / 255.0, green: 136 / 255.0, blue: 103 / 255.0, alpha: 1.00),
+            color2: SKColor(red: 241 / 255.0, green: 188 / 255.0, blue: 151 / 255.0, alpha: 1.00),
+            color3: SKColor(red: 252 / 255.0, green: 53 / 255.0, blue: 113 / 255.0, alpha: 1.00)
         ),
     ]
 
