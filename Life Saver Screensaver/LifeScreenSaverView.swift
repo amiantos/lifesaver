3da671f46ab760fede8982e41a421c50942e5827
amiantos <bradroot@me.com>
2019-06-22 11:46:18 -0700

    add missing license excerpts

--- Life Saver Screensaver/LifeScreenSaverView.swift
+++ Life Saver Screensaver/LifeScreenSaverView.swift
@@ -5,6 +5,9 @@
 //  Created by Bradley Root on 5/18/19.
 //  Copyright © 2019 Brad Root. All rights reserved.
 //
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
 import ScreenSaver
@@ -58,6 +61,7 @@ final class LifeScreenSaverView: ScreenSaverView {
             scene.squareSize = manager.squareSize
             scene.animationSpeed = manager.animationSpeed
             scene.aliveColors = [manager.color1, manager.color2, manager.color3]
+
             scene.isUserInteractionEnabled = false
 
             spriteView.presentScene(scene)
