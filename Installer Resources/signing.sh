4b98ff242ce38041913e8da3822eafe5ee501cae
amiantos <bradroot@me.com>
2019-06-22 12:50:21 -0700

    update installer resources for v1.1

--- Installer Resources/signing.sh
+++ Installer Resources/signing.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/productsign --sign "Developer ID Installer: Brad Root (2Y9M69QJKZ)" lifesaver-1.0-unsigned.pkg lifesaver-1.0.pkg
+/usr/bin/productsign --sign "Developer ID Installer: Brad Root (2Y9M69QJKZ)" lifesaver-1.1-unsigned.pkg lifesaver-1.1.pkg
