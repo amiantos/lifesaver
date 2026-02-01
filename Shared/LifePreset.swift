7fe245d61831d4b018b6c3c050a73bf5a125e3ec
Brad Root <bradroot@me.com>
2026-01-31 19:48:17 -0800

    add new modes to presets

--- Shared/LifePreset.swift
+++ Shared/LifePreset.swift
@@ -19,6 +19,8 @@ struct LifePreset {
     let deathFade: Bool?
     let shiftingColors: Bool?
     let startingPattern: StartingPattern?
+    let gridMode: GridMode?
+    let respawnMode: RespawnMode?
     let color1: SKColor?
     let color2: SKColor?
     let color3: SKColor?
@@ -92,6 +94,8 @@ let settingsPresets = [
         deathFade: true,
         shiftingColors: false,
         startingPattern: .defaultRandom,
+        gridMode: .toroidal,
+        respawnMode: .freshStart,
         color1: SKColor.defaultColor1,
         color2: SKColor.defaultColor2,
         color3: SKColor.defaultColor3
@@ -104,6 +108,8 @@ let settingsPresets = [
         deathFade: false,
         shiftingColors: true,
         startingPattern: .defaultRandom,
+        gridMode: .toroidal,
+        respawnMode: .freshStart,
         color1: SKColor(red: 237 / 255.0, green: 200 / 255.0, blue: 195 / 255.0, alpha: 1.00),
         color2: SKColor(red: 16 / 255.0, green: 103 / 255.0, blue: 110 / 255.0, alpha: 1.00),
         color3: SKColor(red: 247 / 255.0, green: 172 / 255.0, blue: 153 / 255.0, alpha: 1.00)
@@ -116,6 +122,8 @@ let settingsPresets = [
         deathFade: true,
         shiftingColors: false,
         startingPattern: .defaultRandom,
+        gridMode: .toroidal,
+        respawnMode: .freshStart,
         color1: SKColor(red: 0.98, green: 0.75, blue: 0.00, alpha: 1.0),
         color2: SKColor(red: 1.00, green: 0.46, blue: 0.00, alpha: 1.0),
         color3: SKColor(red: 0.71, green: 0.13, blue: 0.01, alpha: 1.0)
@@ -128,6 +136,8 @@ let settingsPresets = [
         deathFade: false,
         shiftingColors: true,
         startingPattern: .defaultRandom,
+        gridMode: .toroidal,
+        respawnMode: .freshStart,
         color1: SKColor(red: 252 / 255.0, green: 98 / 255.0, blue: 101 / 255.0, alpha: 1.00),
         color2: SKColor(red: 88 / 255.0, green: 137 / 255.0, blue: 251 / 255.0, alpha: 1.00),
         color3: SKColor(red: 38 / 255.0, green: 205 / 255.0, blue: 105 / 255.0, alpha: 1.00)
@@ -140,6 +150,8 @@ let settingsPresets = [
         deathFade: true,
         shiftingColors: true,
         startingPattern: .sparse,
+        gridMode: .infinite,
+        respawnMode: .freshStart,
         color1: SKColor(red: 252 / 255.0, green: 98 / 255.0, blue: 101 / 255.0, alpha: 1.00),
         color2: SKColor(red: 88 / 255.0, green: 137 / 255.0, blue: 251 / 255.0, alpha: 1.00),
         color3: SKColor(red: 38 / 255.0, green: 205 / 255.0, blue: 105 / 255.0, alpha: 1.00)
@@ -152,6 +164,8 @@ let settingsPresets = [
         deathFade: true,
         shiftingColors: true,
         startingPattern: .lonelyGliders,
+        gridMode: .toroidal,
+        respawnMode: .freshStart,
         color1: SKColor(red: 252 / 255.0, green: 98 / 255.0, blue: 101 / 255.0, alpha: 1.00),
         color2: SKColor(red: 88 / 255.0, green: 137 / 255.0, blue: 251 / 255.0, alpha: 1.00),
         color3: SKColor(red: 38 / 255.0, green: 205 / 255.0, blue: 105 / 255.0, alpha: 1.00)
@@ -167,6 +181,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor.defaultColor1,
         color2: SKColor.defaultColor2,
         color3: SKColor.defaultColor3
@@ -179,6 +195,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 103 / 255.0, green: 22 / 255.0, blue: 169 / 255.0, alpha: 1.00),
         color2: SKColor(red: 13 / 255.0, green: 17 / 255.0, blue: 108 / 255.0, alpha: 1.00),
         color3: SKColor(red: 12 / 255.0, green: 67 / 255.0, blue: 108 / 255.0, alpha: 1.00)
@@ -191,6 +209,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 113 / 255.0, green: 17 / 255.0, blue: 8 / 255.0, alpha: 1.00),
         color2: SKColor(red: 95 / 255.0, green: 7 / 255.0, blue: 0 / 255.0, alpha: 1.00),
         color3: SKColor(red: 55 / 255.0, green: 55 / 255.0, blue: 55 / 255.0, alpha: 1.00)
@@ -203,6 +223,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 173 / 255.0, green: 255 / 255.0, blue: 14 / 255.0, alpha: 1.00),
         color2: SKColor(red: 174 / 255.0, green: 129 / 255.0, blue: 255 / 255.0, alpha: 1.00),
         color3: SKColor(red: 6 / 255.0, green: 66 / 255.0, blue: 110 / 255.0, alpha: 1.00)
@@ -215,6 +237,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 190 / 255.0, green: 14 / 255.0, blue: 19 / 255.0, alpha: 1.00),
         color2: SKColor(red: 39 / 255.0, green: 65 / 255.0, blue: 110 / 255.0, alpha: 1.00),
         color3: SKColor(red: 212 / 255.0, green: 205 / 255.0, blue: 196 / 255.0, alpha: 1.00)
@@ -227,6 +251,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 255 / 255.0, green: 255 / 255.0, blue: 255 / 255.0, alpha: 1.00),
         color2: SKColor(red: 255 / 255.0, green: 255 / 255.0, blue: 255 / 255.0, alpha: 1.00),
         color3: SKColor(red: 255 / 255.0, green: 255 / 255.0, blue: 255 / 255.0, alpha: 1.00)
@@ -239,6 +265,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 252 / 255.0, green: 98 / 255.0, blue: 101 / 255.0, alpha: 1.00),
         color2: SKColor(red: 88 / 255.0, green: 137 / 255.0, blue: 251 / 255.0, alpha: 1.00),
         color3: SKColor(red: 38 / 255.0, green: 205 / 255.0, blue: 105 / 255.0, alpha: 1.00)
@@ -251,6 +279,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 211 / 255.0, green: 208 / 255.0, blue: 203 / 255.0, alpha: 1.00),
         color2: SKColor(red: 88 / 255.0, green: 123 / 255.0, blue: 127 / 255.0, alpha: 1.00),
         color3: SKColor(red: 255 / 255.0, green: 173 / 255.0, blue: 105 / 255.0, alpha: 1.00)
@@ -263,6 +293,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 229 / 255.0, green: 167 / 255.0, blue: 177 / 255.0, alpha: 1.00),
         color2: SKColor(red: 244 / 255.0, green: 243 / 255.0, blue: 216 / 255.0, alpha: 1.00),
         color3: SKColor(red: 175 / 255.0, green: 211 / 255.0, blue: 213 / 255.0, alpha: 1.00)
@@ -275,6 +307,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 95 / 255.0, green: 67 / 255.0, blue: 107 / 255.0, alpha: 1.00),
         color2: SKColor(red: 205 / 255.0, green: 170 / 255.0, blue: 37 / 255.0, alpha: 1.00),
         color3: SKColor(red: 114 / 255.0, green: 100 / 255.0, blue: 87 / 255.0, alpha: 1.00)
@@ -287,6 +321,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 1 / 255.0, green: 153 / 255.0, blue: 138 / 255.0, alpha: 1.00),
         color2: SKColor(red: 255 / 255.0, green: 203 / 255.0, blue: 213 / 255.0, alpha: 1.00),
         color3: SKColor(red: 191 / 255.0, green: 178 / 255.0, blue: 95 / 255.0, alpha: 1.00)
@@ -299,6 +335,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 208 / 255.0, green: 117 / 255.0, blue: 126 / 255.0, alpha: 1.00),
         color2: SKColor(red: 44 / 255.0, green: 80 / 255.0, blue: 80 / 255.0, alpha: 1.00),
         color3: SKColor(red: 156 / 255.0, green: 154 / 255.0, blue: 23 / 255.0, alpha: 1.00)
@@ -311,6 +349,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 0.98, green: 0.75, blue: 0.00, alpha: 1.0),
         color2: SKColor(red: 1.00, green: 0.46, blue: 0.00, alpha: 1.0),
         color3: SKColor(red: 0.71, green: 0.13, blue: 0.01, alpha: 1.0)
@@ -323,6 +363,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 237 / 255.0, green: 200 / 255.0, blue: 195 / 255.0, alpha: 1.00),
         color2: SKColor(red: 16 / 255.0, green: 103 / 255.0, blue: 110 / 255.0, alpha: 1.00),
         color3: SKColor(red: 247 / 255.0, green: 172 / 255.0, blue: 153 / 255.0, alpha: 1.00)
@@ -335,6 +377,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 122 / 255.0, green: 55 / 255.0, blue: 100 / 255.0, alpha: 1.00),
         color2: SKColor(red: 56 / 255.0, green: 66 / 255.0, blue: 109 / 255.0, alpha: 1.00),
         color3: SKColor(red: 160 / 255.0, green: 121 / 255.0, blue: 72 / 255.0, alpha: 1.00)
@@ -347,6 +391,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 59 / 255.0, green: 67 / 255.0, blue: 72 / 255.0, alpha: 1.00),
         color2: SKColor(red: 247 / 255.0, green: 201 / 255.0, blue: 177 / 255.0, alpha: 1.00),
         color3: SKColor(red: 176 / 255.0, green: 197 / 255.0, blue: 223 / 255.0, alpha: 1.00)
@@ -359,6 +405,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 36 / 255.0, green: 55 / 255.0, blue: 13 / 255.0, alpha: 1.00),
         color2: SKColor(red: 55 / 255.0, green: 60 / 255.0, blue: 13 / 255.0, alpha: 1.00),
         color3: SKColor(red: 10 / 255.0, green: 44 / 255.0, blue: 24 / 255.0, alpha: 1.00)
@@ -371,6 +419,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 243 / 255.0, green: 136 / 255.0, blue: 103 / 255.0, alpha: 1.00),
         color2: SKColor(red: 241 / 255.0, green: 188 / 255.0, blue: 151 / 255.0, alpha: 1.00),
         color3: SKColor(red: 252 / 255.0, green: 53 / 255.0, blue: 113 / 255.0, alpha: 1.00)
@@ -383,6 +433,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 255 / 255, green: 255 / 255, blue: 255 / 255, alpha: 1.0),
         color2: SKColor(red: 220 / 255, green: 220 / 255, blue: 220 / 255, alpha: 1.0),
         color3: SKColor(red: 180 / 255, green: 180 / 255, blue: 180 / 255, alpha: 1.0)
@@ -395,6 +447,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: SKColor(red: 229 / 255, green: 0 / 255, blue: 0 / 255, alpha: 1.0),
         color2: SKColor(red: 204 / 255, green: 0 / 255, blue: 0 / 255, alpha: 1.0),
         color3: SKColor(red: 178 / 255, green: 0 / 255, blue: 0 / 255, alpha: 1.0)
@@ -407,6 +461,8 @@ let colorPresets = [
         deathFade: nil,
         shiftingColors: nil,
         startingPattern: nil,
+        gridMode: nil,
+        respawnMode: nil,
         color1: nil,
         color2: nil,
         color3: nil
